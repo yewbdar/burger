8676bf798a1d65e416ebfcf6c44057fef7acdec2
Yewbdar <Yewbdar.grima@gmail.com>
2018-04-30 12:01:20 -0500

    controller  added

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,17 @@
+var express=require("express");
+var burger=require("../models/burger.js");
+
+var app=express();
+
+app.get("/",function(req,res){
+
+});
+app.post("/",function(req,res){
+
+});
+
+app.delete("/",function(req,res){
+
+});
+
+module.exports=app;
